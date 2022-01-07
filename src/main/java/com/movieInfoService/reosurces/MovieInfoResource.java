d7ad02f37486ec4282fa15af8e76be024295ab48
SusmithaGU <susmitha.gundu@gmail.com>
2022-01-07 12:56:59 +0530

    created movieInfoServie

--- src/main/java/com/movieInfoService/reosurces/MovieInfoResource.java
+++ src/main/java/com/movieInfoService/reosurces/MovieInfoResource.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Collections;
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/movie")
 public class MovieInfoResource {
